f673281c2b381eebfb8ba28de264a4368abe5a8a
Mr-Morgan <zmorgan163@gmail.com>
2023-10-23 20:19:54 +0300

    refactoring encode function

--- yandex_pay/base64.cpp
+++ yandex_pay/base64.cpp
@@ -1,9 +1,10 @@
 #include "base64.h"
 
-char base64_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                     'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+const char base64_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                           'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                           'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+constexpr int two_bit {0x03}, four_bit {0x0f}, six_bit {0x3f};
 
 std::string encode(const std::string& text) {
     // преобразовать входные символы (из каждых трёх символов сделать 4)
@@ -19,39 +20,22 @@ std::string encode(const std::string& text) {
     // третий сдвинуть на 6 бит 3-6 биты взять у предыдущего символа с позиций 1-4
     // четвертый симфол результирующей строки - это первые 6 бит третьего символа
     std::string text_b64 {""};
-    char suffix;
+    char suffix, prefix;
     for (int i = 0; i < text.length(); i++) {
-        char prefix = text[i];
+        prefix = text[i];
         switch((i+1)%3) {
         case 0:
-            prefix >>= 6;
-            text_b64 += base64_map[prefix | suffix];
-
-            suffix = text[i];
-            suffix <<= 2;
-            suffix >>= 2;
-            text_b64 += base64_map[suffix];
+            text_b64 += base64_map[((text[i-1] & four_bit) << 2) + (text[i] >> 6)];
+            text_b64 += base64_map[text[i] & six_bit];
             break;
 
         case 1:
-            prefix >>= 2;
-            text_b64 += base64_map[prefix];
-
-            suffix = text[i];
-            suffix <<= 4;
-            suffix &= ~(1 << 6);
-            suffix &= ~(1 << 7);
+            text_b64 += base64_map[text[i] >> 2];
             if (i == text.length()-1) text_b64 += base64_map[suffix];
             break;
 
         case 2:
-            prefix >>= 4;
-            text_b64 += base64_map[prefix | suffix];
-
-            suffix = text[i];
-            suffix <<= 2;
-            suffix &= ~(1 << 6);
-            suffix &= ~(1 << 7);
+            text_b64 += base64_map[((text[i-1] & two_bit) << 4) + (text[i] >> 4)];
             if (i == text.length()-1) text_b64 += base64_map[suffix];
             break;
         }// switch()
@@ -60,7 +44,7 @@ std::string encode(const std::string& text) {
     for (int i = text.length()%3; i < 3; i++) text_b64 += '=';
 
     return text_b64;
-}// string encode()
+}// std::string encode()
 
 std::string decode(const std::string& text_b64) {
     std::string text {""};
