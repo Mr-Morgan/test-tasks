2690927dddca360670647d36c860cfd394f62df9
Mr-Morgan <zmorgan163@gmail.com>
2023-10-24 12:20:58 +0300

    refactoring decode function, complite test-task|P.S: add new function mapBase64ToIndex in last commit

--- yandex_pay/main.cpp
+++ yandex_pay/main.cpp
@@ -3,8 +3,17 @@
 
 int main ()
 {
-    //std::string text_to_decode {"0JjQt9GD0YfQuNGC0YwgQysrINC30LAgMjEg0LTQtdC90Yw=="};
-    /*TEST CASE*/
+    std::string task_text_to_decode {"0JjQt9GD0YfQuNGC0YwgQysrINC30LAgMjEg0LTQtdC90Yw=="};
+    std::cout << task_text_to_decode << std::endl;
+    std::cout << decode(task_text_to_decode) << std::endl;
+
+/* OUTPUT
+ * 0JjQt9GD0YfQuNGC0YwgQysrINC30LAgMjEg0LTQtdC90Yw==
+ * Изучить C++ за 21 день
+*/
+
+/*
+    //TEST CASE
     std::string text_to_encode {"ABCDEFG HIJKLMNOP"};
     std::string text_to_decode {"QUJDREVGRyBISUpLTE1OT1A="};
 
@@ -18,5 +27,14 @@ int main ()
 
     (decode(text_to_decode) == text_to_encode)? std::cout << "true" << std::endl: std::cout << "false" << std::endl;
 
+    //OUTPUT TEST CASE
+
+    //QUJDREVGRyBISUpLTE1OT1A=
+    //QUJDREVGRyBISUpLTE1OT1A=
+    //true
+    //ABCDEFG HIJKLMNOP
+    //ABCDEFG HIJKLMNOP
+    //true
+*/
     return 0;
 }// int main ()
